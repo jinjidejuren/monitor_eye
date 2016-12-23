3bdc49e574482df6d3ede56f7558856ae5da428a
root <root@server2.example.com>
2016-12-23 15:05:58 +0800

    修改service显示列表

--- service_manage/admin.py
+++ service_manage/admin.py
@@ -4,4 +4,10 @@ from django.contrib import admin
 
 from .models import Service
 
-admin.site.register(Service)
+
+class ServiceAdmin(admin.ModelAdmin):
+    fields = ['service_machine_name', 'inside_ip', 'outside_up','service_group',
+              'cpu_num', 'memory_size', 'disk_size', 'bandwidth', 'create_time' ]
+    list_display = ('service_machine_name', 'outside_up', 'inside_ip', 'create_time')
+
+admin.site.register(Service, ServiceAdmin)
