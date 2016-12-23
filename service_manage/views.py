80f14b3adb0c9b596f94dbbebf4173eb72ace342
root <root@server2.example.com>
2016-12-23 17:28:35 +0800

    使用模板显示视图

--- service_manage/views.py
+++ service_manage/views.py
@@ -4,6 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Service
+from django.template import RequestContext, loader
 
 def index(request):
-    return HttpResponse("欢迎进入MONITOR_EYE!")
+    service_machine_list = Service.objects
+    template = loader.get_template('service_manage/index.html')
+    context = RequestContext(request, {
+        'service_machine_list': service_machine_list,
+    })
+    return HttpResponse(template.render(context))
