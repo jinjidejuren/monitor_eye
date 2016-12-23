08bddb72b69c937d0fd4bfcabf5f7151305d81ad
root <root@server2.example.com>
2016-12-23 14:56:55 +0800

    添加service属性的中文注释

--- service_manage/models.py
+++ service_manage/models.py
@@ -10,11 +10,13 @@ class Service(models.Model):
     inside_ip = models.CharField(u'内网ip', max_length=40)
     outside_up = models.CharField(u'外网ip',max_length=40)
     service_group = models.CharField(u'服务器组', max_length=40)
-    cpu_num = models.IntegerField()
-    memory_size = models.IntegerField()
-    disk_size = models.IntegerField()
-    bandwidth = models.IntegerField()
+    cpu_num = models.IntegerField(u'cpu核数')
+    memory_size = models.IntegerField(u'内存大小（单位:G）')
+    disk_size = models.IntegerField(u'磁盘大小（单位:G）')
+    bandwidth = models.IntegerField(u'带宽（单位:M）')
     create_time = models.DateTimeField(u'创建时间')
 
-    def __unicode__(self):
-        return self.service_machine_name 
+    def __unicode__(self):    #如果是python3可以用__str__
+        return self.service_machine_name
+
+     
