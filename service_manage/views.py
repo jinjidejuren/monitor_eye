5fe7421a0ed4b099849ceca6578e87017b862c42
root <root@server2.example.com>
2016-12-28 22:21:12 +0800

    创建了service_manage主页面的路由

--- service_manage/views.py
+++ service_manage/views.py
@@ -2,15 +2,22 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from django.shortcuts import render_to_response, HttpResponseRedirect
 from django.http import HttpResponse
-from .models import Service
 from django.template import RequestContext, loader
+from service_manage.models import Service
 
-def index(request):
-    service_machine_list = Service.objects
-    template = loader.get_template('service_manage/index.html')
-    context = RequestContext(request, {
-        'service_machine_list': service_machine_list,
-    })
-    return HttpResponse(template.render(context))
+
+# def index(request):
+#     service_machine_list = Service.objects
+#     template = loader.get_template('service_manage/index.html')
+#     context = RequestContext(request, {
+#         'service_machine_list': service_machine_list,
+#     })
+#     return HttpResponse(template.render(context))
+
+
+def service_index(request):
+    service_machine_list = Service.objects.all()
+    service_count = Service.objects.count()
+    return render_to_response('service_manage/service_manage.html', locals())
